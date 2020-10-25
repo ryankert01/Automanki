cb9e70caf939db1d9889de4f7c7ab42c45757db7
YuHsien <ex8600@gmail.com>
2020-10-25 23:06:39 +0800

    Connected the API of adding Japanese notes into Anki

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
 import json
 import crawler.english_cambridge as cambridge
+from Naked.toolshed.shell import muterun_js
 
 DOWNLOAD_DIR = 'C:/Users/Yu-Hsien/AppData/Roaming/Anki2/YuHsien/collection.media/'
-INPUT_FILE = 'input.txt'
+EN_INPUT_FILE = 'input_EN.txt'
+
+JP_CRAWLER_PATH = 'C:/Users/Yu-Hsien/Desktop/crawler/littleD.js'
+JP_OUTPUT_FILE = 'littleDJSON.json'
+
+
 START = 'start'
 END_OF_DICT = '----'
 
@@ -26,62 +32,21 @@ def api (action, **params):
         "params": params,
     }))
 
-payload = {
-    "action": "addNote",
-    "version": 6,
-    "params": {
-        "note": {
-            "deckName": "英文",
-            "modelName": "基本型(含反向的卡片)",
-            "fields": {
-                "正面": "front content",
-                "背面": "back content"
-            },
-            # "options": {
-            #     "allowDuplicate": False,
-            #     "duplicateScope": "deck"
-            # },
-            # "tags": [
-            #     "yomichan"
-            # ],
-            # "audio": [{
-            #     "url": "https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?kanji=猫&kana=ねこ",
-            #     "filename": "yomichan_ねこ_猫.mp3",
-            #     "skipHash": "7e2c2f954ef6051373ba916f000168dc",
-            #     "fields": [
-            #         "正面"
-            #     ]
-            # }]
-        }
-    }
-}
-TARGET = ['[英文]', '[日文單字]', '[日文動詞]']
+# Run JP Crawler
+response = muterun_js(JP_CRAWLER_PATH, 'python')
+if response.exitcode == 0:
+    print(response.stdout.decode("utf-8"))
+else:
+    print(response.stderr)
+with open(JP_OUTPUT_FILE, encoding='utf-8') as output:
+    notes = map(lambda x: { **jp_note_template, "fields": x }, json.load(output))
+    response = api('addNotes', **{ "notes": list(notes) })
+    print('API Response:', response.json())
 
-# # Get deck names
-# response = api('deckNames')
-# deckNames = response.json()['result']
-# print(deckNames)
-
-# # Get model names
-# response = api('modelNames')
-# modelNames = response.json()['result']
-# print(modelNames)
-
-stat = None
-target = None
-with open(INPUT_FILE , encoding='utf-8') as word_list:
+# Run EN Crawler
+with open(EN_INPUT_FILE , encoding='utf-8') as word_list:
     for word in word_list:
         word = word.splitlines()[0]
-        if stat == START:
-            if word != END_OF_DICT:
-                if target == '[英文]':
-                    en_note_template['fields'] = cambridge.LookUp(word, DOWNLOAD_DIR)
-                    response = api('addNote', **{ "note": en_note_template })
-                    print(response.json())
-                else:
-                    print('JP')
-            else:
-                stat = None
-        elif word in TARGET:
-            target = word
-            stat = START
+        en_note_template['fields'] = cambridge.LookUp(word, DOWNLOAD_DIR)
+        response = api('addNote', **{ "note": en_note_template })
+        print('API Response:', response.json())
