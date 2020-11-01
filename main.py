3172d6314ec92bf0c2540124e91ac332ab356431
YuHsien <ex8600@gmail.com>
2020-11-01 17:20:28 +0800

    Added the Japanese sentence input

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ elif platform.system() == 'Darwin':
     JP_CRAWLER_PATH = '/Users/pacsoft/Desktop/littleDCrawler/littleD.js'
 
 EN_INPUT_FILE = 'input_EN.txt'
-JP_OUTPUT_FILE = 'littleDJSON.json'
+JP_INPUT_FILE = 'littleDJSON.json'
+JP_SENTENCE_INPUT_FILE = 'input_JP_sentence.json'
 
 en_note_template = {
     "deckName": "英文",
@@ -26,23 +27,34 @@ jp_note_template = {
     "fields": {},
 }
 
-def api (action, **params):
+jp_sentence_note_template = {
+    "deckName": "日文句子",
+    "modelName": "Japanese (recognition&recall)",
+    "fields": {},
+}
+
+def api(action, **params):
     return requests.post('http://localhost:8765', json.dumps({
         "action": action,
         "version": 6,
         "params": params,
     }))
 
+def add_JP_cards(input_file, note_template):
+    with open(input_file, encoding='utf-8') as json_file:
+        inputs = json.load(json_file)
+        if len(inputs) > 0:
+            notes = map(lambda x: { **note_template, "fields": x }, inputs)
+            response = api('addNotes', **{ "notes": list(notes) })
+            print('API Response:', response.json())
+
 # Run JP Crawler
-response = muterun_js(JP_CRAWLER_PATH, 'python')
-if response.exitcode == 0:
-    print(response.stdout.decode("utf-8"))
+js_response = muterun_js(JP_CRAWLER_PATH, 'python')
+if js_response.exitcode == 0:
+    print(js_response.stdout.decode("utf-8"))
 else:
-    print(response.stderr)
-with open(JP_OUTPUT_FILE, encoding='utf-8') as output:
-    notes = map(lambda x: { **jp_note_template, "fields": x }, json.load(output))
-    response = api('addNotes', **{ "notes": list(notes) })
-    print('API Response:', response.json())
+    print(js_response.stderr)
+add_JP_cards(JP_INPUT_FILE, jp_note_template)
 
 # Run EN Crawler
 with open(EN_INPUT_FILE , encoding='utf-8') as word_list:
@@ -51,3 +63,6 @@ with open(EN_INPUT_FILE , encoding='utf-8') as word_list:
         en_note_template['fields'] = cambridge.LookUp(word, DOWNLOAD_DIR)
         response = api('addNote', **{ "note": en_note_template })
         print('API Response:', response.json())
+
+# Run JP Sentence Input
+add_JP_cards(JP_SENTENCE_INPUT_FILE, jp_sentence_note_template)
